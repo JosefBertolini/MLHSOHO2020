9239b6d3cd64aba9e706a772241f0d1abef5cd28
JosefBertolini <39634656+JosefBertolini@users.noreply.github.com>
2020-08-29 21:21:29 -0700

    teams.js

--- app/controllers/teams.js
+++ app/controllers/teams.js
@@ -37,8 +37,48 @@ router.get("/", function(req, res) {
                 members += "<p id=p"+id+">" + hacker["name"] + "<br>Favorite Language: " + hacker["favorite_languages"] + "<br><button type=\"button\">Invite</button>" +"</p>\n";
             }
         }
-        members += '</div>'
+        members += '</div><div id="teams">'
         
+        fs.readFile(".\\app\\resources\\teams.json", "utf-8", (errT, teams) => {
+            if (err) throw err;
+            var teamObj = JSON.parse(teams);
+
+            var hack_team= "";
+            switch (hack_name)
+            {   
+                case "Suns Out Hacks Out":
+                    hack_team = "SOHO teams";
+                    break;
+                case "First Day Back Hacks":
+                    hack_team = "FDBH teams";
+                    break;
+                case "MedHacks":
+                    hack_team = "MedH teams";
+                    break;
+            }
+
+            var teams = teamObj[hack_team];
+
+            for (var team_num in teams)
+            {
+                team = teams[team_num];
+                if (team.length > 0) {
+                    members += '<div id="team'+team_num+'">';
+
+                    for (var hacker in team)
+                    {
+                        hacker = team[hacker];
+                        members += "<p>" + hacker["name"] + "<br>Favorite Language: " + hacker["favorite_languages"] + "</p>"
+                    }
+
+                    members += '</div>';
+                }
+            }
+
+
+        })
+
+        members += "</div>"
         //console.log(members);
 
         fs.readFile(".\\app\\templates\\teams\\index.html", "utf-8", (err2, data) => {
